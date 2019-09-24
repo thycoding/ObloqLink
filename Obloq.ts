234aaf1e540912ef3ae54c2154bb6280a23e41ce
thycoding <49593576+thycoding@users.noreply.github.com>
2019-09-25 01:33:12 +0800

    Update Obloq.ts

--- Obloq.ts
+++ Obloq.ts
@@ -882,7 +882,7 @@ namespace Obloq {
      * time(ms): private long maxWait
      * @param time set timeout, eg: 10000
     */
-    //% weight=77
+    //% weight=45
     //% blockId=saveToThingSpeak
     //% expandableArgumentMode"toggle" inlineInputMode=inline
     //% block="send data to ThingSpeak :| write key: %myKey timeout(ms): %time  field1: %field1 || field2: %field2 field3: %field3 field4: %field4 field5: %field5 field6: %field6 field7: %field7 field8: %field8"
