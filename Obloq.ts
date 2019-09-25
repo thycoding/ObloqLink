55614e6e95695c6470b81312e2b307d7ab4acca3
thycoding <49593576+thycoding@users.noreply.github.com>
2019-09-26 01:42:36 +0800

    Update Obloq.ts

--- Obloq.ts
+++ Obloq.ts
@@ -857,7 +857,7 @@ namespace Obloq {
      * Connect to https://thingspeak.com/ to store the data from micro:bit
     */
     //% weight=100
-    //% blockId=saveToThingSpeak blockGap=9
+    //% blockId=saveToThingSpeak blockGap=5
     //% expandableArgumentMode"toggle" inlineInputMode=inline
     //% block="send data to ThingSpeak :| write key: %myKey field1: %field1 || field2: %field2 field3: %field3 field4: %field4 field5: %field5 field6: %field6 field7: %field7 field8: %field8" group="04_ThingSpeak"
     //% advanced=true
