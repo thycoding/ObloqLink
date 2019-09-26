274498eac036b88f004ebc01f8bf1ef58df8fce4
thycoding <49593576+thycoding@users.noreply.github.com>
2019-09-26 18:41:11 +0800

    Update Obloq.ts

--- Obloq.ts
+++ Obloq.ts
@@ -861,9 +861,9 @@ namespace Obloq {
     //% weight=100
     //% blockId=Obloq_ThingSpeak_post blockGap=5
     //% expandableArgumentMode"toggle" inlineInputMode=inline    
-    //% block="ThingSpeak(post) | write key: %myKey field1 %field1| field2 %field2| field3 %field3| field4 %field4| field5 %field5| field6 %field6| timeout(ms) %time" group="04_ThingSpeak"
+    //% block="ThingSpeak(post) | write key: %myKey field1 %field1| field2 %field2| field3 %field3| field4 %field4| field5 %field5| field6 %field6| field7 %field7| field8 %field8| timeout(ms) %time" group="04_ThingSpeak"
     //% advanced=true        
-    export function Obloq_ThingSpeak_post(myKey: string, field1?: number, field2?: number, field3?: number, field4?: number, field5?: number, field6?: number, time?: number): string {
+    export function Obloq_ThingSpeak_post(myKey: string, field1?: number, field2?: number, field3?: number, field4?: number, field5?: number, field6?: number, field7?: number, field8?: number, time?: number): string {
         while (OBLOQ_WORKING_MODE_IS_STOP) { basic.pause(20) }
         if (!OBLOQ_HTTP_INIT)
             return OBLOQ_STR_TYPE_IS_NONE
@@ -871,11 +871,21 @@ namespace Obloq {
         if (!OBLOQ_SERIAL_INIT) {
             Obloq_serial_init()
         }
-        obloqWriteString("|3|2|http://api.thingspeak.com/update?api_key=" + myKey + "&field1="+field1+"&field2="+field2+"&field3="+field3+"&field4="+field4+"&field5="+field5+"&field6="+field6 + "|\r")
+        let myArr:number[]=[field1,field2,field3,field4,field5,field6,field7,field8]
+        let myUrl = "http://api.thingspeak.com/update?api_key=" + myKey
+        for(let i=0;i<myArr.length;i++)
+        {
+            if (myArr[i]!=null)
+                myUrl+="&field"+(i+1)+"="+myArr[i]
+            else
+                break
+        }	   
+        // obloqWriteString("|3|2|http://api.thingspeak.com/update?api_key=" + myKey + "&field1="+field1+"&field2="+field2+"&field3="+field3+"&field4="+field4+"&field5="+field5+"&field6="+field6 + "|\r")
+        obloqWriteString("|3|1|" + myUrl + "|\r")	   
         let ret = Obloq_http_wait_request(time)
         return ret
     }    
-    
+
     
     /**
      * The HTTP request.url(string):URL:time(ms): private long maxWait
