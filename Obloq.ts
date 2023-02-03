f61d8ef6dee4f339e77727ae808537e4086e3a25
thycoding <49593576+thycoding@users.noreply.github.com>
2023-02-03 20:49:36 +0800

    Update Obloq.ts

--- Obloq.ts
+++ Obloq.ts
@@ -926,7 +926,7 @@ namespace Obloq {
             Obloq_serial_init()
         }
 	   
-        obloqWriteString("|3|1|http://" + url + "|\r")
+        obloqWriteString("|3|1|https://" + url + "|\r")
         let ret = Obloq_http_wait_request(time)
         if (ret == "")
             basic.showIcon(IconNames.No)
